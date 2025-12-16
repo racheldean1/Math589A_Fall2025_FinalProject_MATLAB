69191ec141dacfe0ef1c075fc21f739207a8d620
racheldean1 <racheldean1@arizona.edu>
2025-12-16 13:05:46 -0700

    Update svd_features.m

--- svd_features.m
+++ svd_features.m
@@ -19,5 +19,34 @@ function feat = svd_features(image, p)
 %     r_0_9  = min{k : E(k) >= 0.9}
 %     r_0_95 = min{k : E(k) >= 0.95}
 
-error('svd_features not implemented yet.');
+    [~, S, ~] = svd(image, 'econ');
+    sigma = diag(S);
+
+    % Normalize the singular value
+    ssum = sum(sigma);
+    if ssum == 0
+        normalized = zeros(size(sigma));
+    else
+        normalized = sigma / ssum;
+    end
+
+    % Getting total energy
+    sig2 = sigma.^2;
+    total_energy = sum(sig2);
+    if total_energy == 0
+        cumE = zeros(size(sig2));
+    else
+        cumE = cumsum(sig2) / total_energy;
+    end
+
+    % Effective ranks - 0.9 and 0.95
+    r_0_9 = find(cumE >= 0.9, 1, 'first');
+    r_0_95 = find(cumE >= 0.95, 1, 'first');
+
+    % Safety
+    if isempty(r_0_9),  r_0_9  = length(sigma); end
+    if isempty(r_0_95), r_0_95 = length(sigma); end
+
+    feat = [normalized(1:p); r_0_9; r_0_95];
 end
+
