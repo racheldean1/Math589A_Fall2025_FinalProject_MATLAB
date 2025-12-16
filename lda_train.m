3eabd7dae4a8d57cf3564fd943e7949962063b0d
racheldean1 <racheldean1@arizona.edu>
2025-12-16 13:04:47 -0700

    Update lda_train.m

--- lda_train.m
+++ lda_train.m
@@ -19,5 +19,34 @@ function [w, threshold] = lda_train(X, y)
 %     - Let m0, m1 be means of z for classes 0 and 1, respectively.
 %     - Set threshold = (m0 + m1) / 2.
 
-error('lda_train not implemented yet.');
+    y = y(:);
+
+    X0 = X(y == 0, :);
+    X1 = X(y == 1, :);
+
+    % making the means column vectors
+    mu0 = mean(X0, 1).';
+    mu1 = mean(X1, 1).';
+
+    % separating X into X0 and X1 by class
+    X0c = X0 - mu0.'; % n0 x d
+    X1c = X1 - mu1.'; % n1 x d
+    S_W = (X0c.' * X0c) + (X1c.' * X1c); % d x d
+
+    d = size(X, 2);
+
+    % Small regularization 
+    if rcond(S_W) < 1e-12
+        reg = 1e-6 * (trace(S_W) / max(d,1) + 1);
+        S_W = S_W + reg * eye(d);
+    end
+
+    % Solve S_W * w = (mu1 - mu0)
+    w = S_W \ (mu1 - mu0);
+
+    % Project training data 
+    z = X * w;  % N x 1
+    m0 = mean(z(y == 0)); % setting means
+    m1 = mean(z(y == 1));
+    threshold = (m0 + m1) / 2;  % setting threshold
 end
